87b7aaf0383553eae91cac438d5c4f1d5001d9f5
Andrey Wampir <wampirchikkkk@yandex.ru>
2020-03-21 16:39:02 +0500

    style: use constant for repeated statement

--- test/base.ts
+++ test/base.ts
@@ -5,17 +5,19 @@ import {
   RequiredApiKeyError, RequiredSessionIDError,
 } from '../src/errors';
 
+const TMDB_API_KEY = process.env.TMDB_API_KEY as string;
+
 test('instance creating', t => {
   t.throws(() => new MovieDB({ apiKey: '' }), { instanceOf: RequiredApiKeyError });
-  t.notThrows(() => new MovieDB({ apiKey: process.env.TMDB_API_KEY as string }));
+  t.notThrows(() => new MovieDB({ apiKey: TMDB_API_KEY }));
 });
 
 test('base functions', t => {
-  const mdb = new MovieDB({ apiKey: process.env.TMDB_API_KEY as string });
+  const mdb = new MovieDB({ apiKey: TMDB_API_KEY });
 
   // Api key
   t.throws(() => mdb.setApiKey(''), { instanceOf: RequiredApiKeyError });
-  t.notThrows(() => mdb.setApiKey(process.env.TMDB_API_KEY as string));
+  t.notThrows(() => mdb.setApiKey(TMDB_API_KEY));
 
   // Language
   t.notThrows(() => mdb.setLanguage('ru-RU'));
