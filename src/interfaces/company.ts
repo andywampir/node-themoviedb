d878cd882264408faf6c107165e9fb47a6a88f19
Andrey Wampir <wampirchikkkk@yandex.ru>
2020-04-14 00:08:40 +0500

    chore: add interfaces for company v3

---
+++ src/interfaces/company.ts
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+import { Image } from './common';
+
+// Return types
+export interface CompanyReturnType {
+  details?: CompanyDetails[];
+  alternativeNames?: CompanyAlternativeNames[];
+  images?: CompanyImages[];
+}
+
+interface CompanyDetails {
+  description: string;
+  headquarters: string;
+  homepage: string;
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+  parent_company: CompanyDetails;
+}
+
+interface CompanyAlternativeNames {
+  id: number;
+  results: {
+    name: string;
+    type: string;
+  }[];
+}
+
+interface CompanyImages {
+  id: number;
+  logos: CompanyLogo[];
+}
+
+interface CompanyLogo extends Image {
+  id: string;
+  file_type: '.svg' | '.png';
+}
