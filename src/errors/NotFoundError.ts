b876177114c507dae325cae2b32e751a4be95606
Andrey Wampir <wampirchikkkk@yandex.ru>
2020-06-09 23:07:56 +0500

    fix(errors): typo

--- src/errors/NotFoundError.ts
+++ src/errors/NotFoundError.ts
@@ -2,7 +2,7 @@ export default class NotFoundError extends Error {
   public code: number;
 
   public constructor(code: number) {
-    super('The resource you requested could not be found');
+    super('The resource you request could not be found');
 
     this.code = code;
   }
