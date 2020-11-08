b7271e2e1b48148956b38286b1109251c66db7fc
Andrey Wampir <wampirchikkkk@yandex.ru>
2020-11-08 19:10:24 +0500

    ✨ utils: update user agent version to 0.2.5

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -2,7 +2,7 @@ import got from 'got';
 
 const request = got.extend({
   prefixUrl: 'https://api.themoviedb.org/3',
-  headers: { 'user-agent': 'node-moviedb/0.2.3' },
+  headers: { 'user-agent': 'node-moviedb/0.2.5' },
 });
 
 export default request;
