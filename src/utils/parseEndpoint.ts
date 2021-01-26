fb45622548aeb3d4e9c0daab1bdea3b5f826f7ea
Andrey Wampir <wampirchikkkk@yandex.ru>
2021-01-26 20:57:44 +0500

    refactor: remove eslint-ignore for rule no-cond-assign

--- src/utils/parseEndpoint.ts
+++ src/utils/parseEndpoint.ts
@@ -1,11 +1,12 @@
 export default (endpoint: string, params: Record<string, string>): string => {
   const regExp = /{(.+?)}/;
   let parsedEndpoint = endpoint;
-  let match: RegExpExecArray | null = null;
+  let match: RegExpExecArray | null = regExp.exec(parsedEndpoint);
 
-  // eslint-disable-next-line no-cond-assign
-  while (match = regExp.exec(parsedEndpoint))
+  while (match) {
     parsedEndpoint = parsedEndpoint.replace(match[0], params[match[1]]);
+    match = regExp.exec(parsedEndpoint);
+  }
 
   return parsedEndpoint;
 };
