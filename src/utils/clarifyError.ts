41171ba498e2b949c1bbad35aab9219ff6f22c90
Andrey Wampir <wampirchikkkk@yandex.ru>
2021-02-02 22:44:44 +0500

    🔥 utils: remove unused file

--- src/utils/clarifyError.ts
+++
@@ -1,9 +0,0 @@
-import type { BeforeErrorHook } from 'got';
-
-import * as errors from '../errors';
-
-const clarifyError: BeforeErrorHook = error => {
-  switch (error.)
-};
-
-export default clarifyError;
