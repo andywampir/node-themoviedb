ba84f6e897b8738e2b2bb0121c93adcaa013279d
Andrey Wampir <wampirchikkkk@yandex.ru>
2021-02-03 22:18:51 +0500

    🏷️ types: refactor some endpoints' types for new client

--- src/interfaces/v3/companies.ts
+++ src/interfaces/v3/companies.ts
@@ -1,44 +1,57 @@
-/* eslint-disable camelcase */
-import { Image } from '../common';
+import type { IClient } from '../../utils/Client';
+import type { Image } from '../common';
 
-// Options
-export interface CompaniesConstructorOptions {
-	apiKey: string;
-	companyID?: number;
+namespace CompaniesEndpointNS {
+	export interface Class {
+		details(companyID?: number): Promise<Results.Details>;
+		alternativeNames(companyID?: number): Promise<Results.AlternativeNames>;
+		images(companyID?: number): Promise<Results.Images>;
+	}
+
+	export namespace Options {
+		export interface Constructor {
+			apiKey: string;
+			client: IClient;
+			companyID?: number;
+		}
+	}
+
+	export namespace Results {
+		export type Details = Types.Details[];
+		export type AlternativeNames = Types.AlternativeNames[];
+		export type Images = Types.Images[];
+	}
+
+	namespace Types {
+		export interface Details {
+			description: string;
+			headquarters: string;
+			homepage: string;
+			id: number;
+			logo_path: string;
+			name: string;
+			origin_country: string;
+			parent_company: Details;
+		}
+
+		export interface AlternativeNames {
+			id: number;
+			results: {
+				name: string;
+				type: string;
+			}[];
+		}
+
+		export interface Images {
+			id: number;
+			logos: Logo[];
+		}
+
+		interface Logo extends Image {
+			id: string;
+			file_type: '.svg' | '.png';
+		}
+	}
 }
 
-// Return types
-export interface CompaniesReturnType {
-	details?: CompaniesDetails[];
-	alternativeNames?: CompaniesAlternativeNames[];
-	images?: CompaniesImages[];
-}
-
-interface CompaniesDetails {
-	description: string;
-	headquarters: string;
-	homepage: string;
-	id: number;
-	logo_path: string;
-	name: string;
-	origin_country: string;
-	parent_company: CompaniesDetails;
-}
-
-interface CompaniesAlternativeNames {
-	id: number;
-	results: {
-		name: string;
-		type: string;
-	}[];
-}
-
-interface CompaniesImages {
-	id: number;
-	logos: CompaniesLogo[];
-}
-
-interface CompaniesLogo extends Image {
-	id: string;
-	file_type: '.svg' | '.png';
-}
+export default CompaniesEndpointNS;
