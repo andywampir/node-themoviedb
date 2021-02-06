6339ef8e2b6c624a751345eb7437e4514b297a54
Andrey Wampir <wampirchikkkk@yandex.ru>
2021-02-06 11:50:04 +0500

    🏷️ types: refactor some endpoints' types for new client

--- src/interfaces/v3/discover.ts
+++ src/interfaces/v3/discover.ts
@@ -1,94 +1,107 @@
-/* eslint-disable camelcase */
-import {
-	ResultsWithPage, Movie,
+import type { IClient } from '../../utils/Client';
+import type {
+	ResultsWithPage, Movie as IMovie,
 	TVShow,
 } from '../common';
 
-// Options
-export interface DiscoverConstructorOptions {
-	apiKey: string;
-	language: string;
+namespace DiscoverEndpointNS {
+	export interface Class {
+		movie(options?: Options.Movie): Promise<Results.Movie>;
+		tv(options?: Options.TV): Promise<Results.TV>;
+	}
+
+	export namespace Options {
+		export interface Constructor {
+			apiKey: string;
+			language: string;
+			client: IClient;
+		}
+
+		type CommonSortBy =
+			| 'popularity.asc'
+			| 'popularity.desc'
+			| 'vote_average.asc'
+			| 'vote_average.desc';
+
+		interface CommonOptions<TSortBy> {
+			language?: string;
+			sortBy?: TSortBy;
+			page?: number;
+			voteCountGTE?: number;
+			voteAverageGTE?: number;
+			withGenres?: string;
+			withRuntimeLTE?: number;
+			withRuntimeGTE?: number;
+			withOriginalLanguage?: string;
+			withCompanies?: string;
+			withKeywords?: string;
+			withoutKeywords?: string;
+			withoutGenres?: string;
+		}
+
+		type MovieSortBy =
+			| CommonSortBy
+			| 'release_date.asc'
+			| 'release_date.desc'
+			| 'revenue.asc'
+			| 'revenue.desc'
+			| 'primary_release_date.asc'
+			| 'primary_release_date.desc'
+			| 'original_title.asc'
+			| 'original_title.desc'
+			| 'vote_count.asc'
+			| 'vote_count.desc';
+
+		export interface Movie extends CommonOptions<MovieSortBy> {
+			region?: string;
+			certificationCountry?: string;
+			certification?: string;
+			certificationLTE?: string;
+			certificationGTE?: string;
+			includeAdult?: boolean;
+			includeVideo?: boolean;
+			primaryReleaseYear?: number;
+			primaryReleaseDateLTE?: string;
+			primaryReleaseDateGTE?: string;
+			releaseDateLTE?: string;
+			releaseDateGTE?: string;
+			withReleaseType?: string;
+			year?: number;
+			voteCountLTE?: number;
+			voteAverageLTE?: number;
+			withCast?: string;
+			withCrew?: string;
+			withPeople?: string;
+		}
+
+		type TVSortBy =
+			| CommonSortBy
+			| 'first_air_date.asc'
+			| 'first_air_date.desc';
+
+		export interface TV extends CommonOptions<TVSortBy> {
+			airDateLTE?: string;
+			airDateGTE?: string;
+			firstAirDateLTE?: string;
+			firstAirDateGTE?: string;
+			firstAirDateYear?: number;
+			timezone?: string;
+			withNetworks?: string;
+			includeNullFirstAirDates?: boolean;
+			screenedTheatrically?: boolean;
+		}
+	}
+
+	export namespace Results {
+		export type Movie = Types.Movie;
+		export type TV = Types.TV;
+	}
+
+	namespace Types {
+		export interface Movie extends ResultsWithPage<IMovie> {}
+
+		export interface TV extends ResultsWithPage<TVShow> {}
+	}
 }
 
-type CommonSortBy =
-| 'popularity.asc'
-| 'popularity.desc'
-| 'vote_average.asc'
-| 'vote_average.desc';
-
-interface CommonParameters<TSortBy> {
-	language?: string;
-	sortBy?: TSortBy;
-	page?: number;
-	voteCountGTE?: number;
-	voteAverageGTE?: number;
-	withGenres?: string;
-	withRuntimeLTE?: number;
-	withRuntimeGTE?: number;
-	withOriginalLanguage?: string;
-	withCompanies?: string;
-	withKeywords?: string;
-	withoutKeywords?: string;
-	withoutGenres?: string;
-}
-
-export interface DiscoverMovieOptions extends CommonParameters<DiscoverMovieSortBy> {
-	region?: string;
-	certificationCountry?: string;
-	certification?: string;
-	certificationLTE?: string;
-	certificationGTE?: string;
-	includeAdult?: boolean;
-	includeVideo?: boolean;
-	primaryReleaseYear?: number;
-	primaryReleaseDateLTE?: string;
-	primaryReleaseDateGTE?: string;
-	releaseDateLTE?: string;
-	releaseDateGTE?: string;
-	withReleaseType?: string;
-	year?: number;
-	voteCountLTE?: number;
-	voteAverageLTE?: number;
-	withCast?: string;
-	withCrew?: string;
-	withPeople?: string;
-}
-
-type DiscoverMovieSortBy =
-| CommonSortBy
-| 'release_date.asc'
-| 'release_date.desc'
-| 'revenue.asc'
-| 'revenue.desc'
-| 'primary_release_date.asc'
-| 'primary_release_date.desc'
-| 'original_title.asc'
-| 'original_title.desc'
-| 'vote_count.asc'
-| 'vote_count.desc';
-
-export interface DiscoverTVOptions extends CommonParameters<DiscoverTVSortBy> {
-	airDateLTE?: string;
-	airDateGTE?: string;
-	firstAirDateLTE?: string;
-	firstAirDateGTE?: string;
-	firstAirDateYear?: number;
-	timezone?: string;
-	withNetworks?: string;
-	includeNullFirstAirDates?: boolean;
-	screenedTheatrically?: boolean;
-}
-
-type DiscoverTVSortBy =
-| CommonSortBy
-| 'first_air_date.asc'
-| 'first_air_date.desc';
-
-// Return Types
-export interface DiscoverReturnType {
-	movie?: DiscoverMovie[];
-	tv?: DiscoverTV[];
-}
-
-interface DiscoverMovie extends ResultsWithPage<Movie> {}
-interface DiscoverTV extends ResultsWithPage<TVShow> {}
+export default DiscoverEndpointNS;
