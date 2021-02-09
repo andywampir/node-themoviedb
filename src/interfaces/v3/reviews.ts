cc831ea34ea88c2bed2ba818aa5a09815d90b1c7
Andrey Wampir <wampirchikkkk@yandex.ru>
2021-02-09 23:26:08 +0500

    🏷️ types: refactor some endpoints' types for new client

--- src/interfaces/v3/reviews.ts
+++ src/interfaces/v3/reviews.ts
@@ -1,17 +1,33 @@
-/* eslint-disable camelcase */
+import type { IClient } from '../../utils/Client';
 
-// Return Types
-export interface ReviewsReturnType {
-	get?: ReviewsGet[];
+namespace ReviewsEndpointNS {
+	export interface Class {
+		get(reviewID: number): Promise<Results.Get>;
+	}
+
+	export namespace Options {
+		export interface Constructor {
+			apiKey: string;
+			client: IClient;
+		}
+	}
+
+	export namespace Results {
+		export type Get = Types.Get;
+	}
+
+	namespace Types {
+		export interface Get {
+			id: string;
+			author: string;
+			content: string;
+			iso_639_1: string;
+			media_id: number;
+			media_title: string;
+			media_type: string;
+			url: string;
+		}
+	}
 }
 
-interface ReviewsGet {
-	id: string;
-	author: string;
-	content: string;
-	iso_639_1: string;
-	media_id: number;
-	media_title: string;
-	media_type: string;
-	url: string;
-}
+export default ReviewsEndpointNS;
--- src/interfaces/v3/trending.ts
+++ src/interfaces/v3/trending.ts
@@ -1,23 +1,34 @@
-/* eslint-disable camelcase */
-import {
+import type { IClient } from '../../utils/Client';
+import type {
 	ResultsWithPage, Movie,
 } from '../common';
 
-// Options
-export interface TrendingGetOptions {
-	mediaType:
-	| 'all'
-	| 'movie'
-	| 'tv'
-	| 'person';
-	timeWindow:
-	| 'day'
-	| 'week';
+namespace TrendingEndpointNS {
+	export interface Class {
+		get(options: Options.Get): Promise<Results.Get>;
+	}
+
+	export namespace Options {
+		export interface Constructor {
+			apiKey: string;
+			client: IClient;
+		}
+
+		export interface Get {
+			mediaType:
+			| 'all'
+			| 'movie'
+			| 'tv'
+			| 'person';
+			timeWindow:
+			| 'day'
+			| 'week';
+		}
+	}
+
+	export namespace Results {
+		export type Get = ResultsWithPage<Movie>;
+	}
 }
 
-// Return Types
-export interface TrendingReturnType {
-	get?: TrendingGet[];
-}
-
-interface TrendingGet extends ResultsWithPage<Movie> {}
+export default TrendingEndpointNS;
