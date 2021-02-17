3afebdef12a4e6f5f4c405cff2481210a453e69c
Andrey Wampir <wampirchikkkk@yandex.ru>
2021-02-17 22:09:21 +0500

    🚧 wip: add new endpoints in moviedb instance

--- src/index.ts
+++ src/index.ts
@@ -5,33 +5,25 @@ import AuthentificationEndpoint from './endpoints/v3/AuthenticationEndpoint';
 import CertificationsEndpoint from './endpoints/v3/CertificationsEndpoint';
 import ChangesEndpoint from './endpoints/v3/ChangesEndpoint';
 import CollectionsEndpoint from './endpoints/v3/CollectionsEndpoint';
+import CompaniesEndpoint from './endpoints/v3/CompaniesEndpoint';
+import ConfigurationEndpoint from './endpoints/v3/ConfigurationEndpoint';
+import CreditsEndpoint from './endpoints/v3/CreditsEndpoint';
+import DiscoverEndpoint from './endpoints/v3/DiscoverEndpoint';
+import FindEndpoint from './endpoints/v3/FindEndpoints';
+import GenresEndpoint from './endpoints/v3/GenresEndpoint';
 
 import { RequiredParameterError } from './errors';
 
-interface AccountEndpointOptions {
-	sessionID?: string;
-	language?: string;
-	userID?: number;
-}
+import MovieDBNS from './interfaces/moviedb';
 
-interface CollectionEndpointOptions {
-	collectionID?: number;
-	language?: string;
-}
-
-interface MovieDBConstructorOptions {
-	apiKey: string;
-	language?: string;
-}
-
-export default class MovieDB {
+export default class MovieDB implements MovieDBNS.Class {
 	private apiKey: string;
 	private language: string;
 	private sessionID?: string;
 	private readonly clientV3: IClient;
 	private readonly clientV4: IClient;
 
-	public constructor(options: MovieDBConstructorOptions) {
+	public constructor(options: MovieDBNS.Options.Constructor) {
 		if (!options.apiKey)
 			throw new RequiredParameterError('apiKey');
 
@@ -59,7 +51,7 @@ export default class MovieDB {
 		this.sessionID = sessionID;
 	}
 
-	public account(options?: AccountEndpointOptions): AccountEndpoint {
+	public account(options?: MovieDBNS.Options.V3.Account): AccountEndpoint {
 		return new AccountEndpoint({
 			apiKey: this.apiKey,
 			sessionID: options?.sessionID ?? this.sessionID,
@@ -90,12 +82,59 @@ export default class MovieDB {
 		});
 	}
 
-	public collections(options?: CollectionEndpointOptions): CollectionsEndpoint {
+	public collections(options?: MovieDBNS.Options.V3.Collections): CollectionsEndpoint {
 		return new CollectionsEndpoint({
 			apiKey: this.apiKey,
 			language: options?.language ?? this.language,
 			collectionID: options?.collectionID,
 			client: this.clientV3,
+		});
+	}
+
+	public companies(options?: MovieDBNS.Options.V3.Companies): CompaniesEndpoint {
+		return new CompaniesEndpoint({
+			apiKey: this.apiKey,
+			client: this.clientV3,
+			companyID: options?.companyID,
+		});
+	}
+
+	public configuration(): ConfigurationEndpoint {
+		return new ConfigurationEndpoint({
+			apiKey: this.apiKey,
+			client: this.clientV3,
+		});
+	}
+
+	public credits(options?: MovieDBNS.Options.V3.Credits): CreditsEndpoint {
+		return new CreditsEndpoint({
+			apiKey: this.apiKey,
+			client: this.clientV3,
+			creditID: options?.creditID,
+		});
+	}
+
+	public discover(options?: MovieDBNS.Options.V3.Discover): DiscoverEndpoint {
+		return new DiscoverEndpoint({
+			apiKey: this.apiKey,
+			client: this.clientV3,
+			language: options?.language ?? this.language,
+		});
+	}
+
+	public find(options?: MovieDBNS.Options.V3.Find): FindEndpoint {
+		return new FindEndpoint({
+			apiKey: this.apiKey,
+			client: this.clientV3,
+			language: options?.language ?? this.language,
+		});
+	}
+
+	public genres(options?: MovieDBNS.Options.V3.Genres): GenresEndpoint {
+		return new GenresEndpoint({
+			apiKey: this.apiKey,
+			client: this.clientV3,
+			language: options?.language ?? this.language,
 		});
 	}
 }
