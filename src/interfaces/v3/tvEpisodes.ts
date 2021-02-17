7c4746d61a16a029d501026b893f45f0863a5337
Andrey Wampir <wampirchikkkk@yandex.ru>
2021-02-17 22:06:22 +0500

    🏷️ types: add language to tvEpisodes constructor

--- src/interfaces/v3/tvEpisodes.ts
+++ src/interfaces/v3/tvEpisodes.ts
@@ -24,6 +24,7 @@ namespace TVEpisodesEndpointNS {
 		export interface Constructor {
 			apiKey: string;
 			client: IClient;
+			language: string;
 			tvID?: number;
 			seasonNumber?: number;
 			episodeNumber?: number;
