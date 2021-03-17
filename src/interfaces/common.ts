92e8892f24e2aa4f69b7c0f44f667ae6b5d4500d
Andrey Wampir <wampirchikkkk@yandex.ru>
2021-03-17 22:45:52 +0500

    🏷️ types: add `ResponseWithCodeExtended` interface

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -16,6 +16,10 @@ export interface ResponseWithCode {
 	status_code: number;
 }
 
+export interface ResponseWithCodeExtended extends ResponseWithCode {
+	success: boolean;
+}
+
 export type SearchParametrs = Record<string, string | number | boolean | null | undefined>;
 export type MediaType = 'movie' | 'tv';
 export type SortOrder = 'created_at.asc' | 'created_at.desc';
