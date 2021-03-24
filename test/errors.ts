85d10cf0bbed4ccbe3c8fd2b8a7d3dfc022aa960
Andrey Wampir <wampirchikkkk@yandex.ru>
2021-03-24 20:52:29 +0500

    ✅ tests: add test for `InternalServerError` error

--- test/errors.ts
+++ test/errors.ts
@@ -2,25 +2,29 @@ import test from 'ava';
 
 import * as errors from '../src/errors';
 
-test('not enough permission error', t => {
+test('[error] not enough permission error', t => {
 	const notEnoughPermissionError = t.throws<errors.NotEnoughPermissionError>(() => {
 		throw new errors.NotEnoughPermissionError('you can not touch this', 1337);
 	}, { instanceOf: errors.NotEnoughPermissionError });
 
 	t.is(notEnoughPermissionError.message, 'you can not touch this');
-	t.is(notEnoughPermissionError.code, 1337);
+	t.is(notEnoughPermissionError.statusCode, 1337);
+	t.is(notEnoughPermissionError.httpCode, 401);
+	t.is(notEnoughPermissionError.shortMessage, 'Not Enough Permission');
 });
 
-test('not found error', t => {
+test('[error] not found error', t => {
 	const notFoundError = t.throws<errors.NotFoundError>(() => {
 		throw new errors.NotFoundError(1337);
 	}, { instanceOf: errors.NotFoundError });
 
-	t.is(notFoundError.code, 1337);
-	t.is(notFoundError.message, 'The resource you requested could not be found');
+	t.is(notFoundError.statusCode, 1337);
+	t.is(notFoundError.message, 'The resource you request could not be found');
+	t.is(notFoundError.httpCode, 404);
+	t.is(notFoundError.shortMessage, 'Not Found');
 });
 
-test('required parameter', t => {
+test('[error] required parameter error', t => {
 	const requiredParameterError = t.throws<errors.RequiredParameterError>(() => {
 		throw new errors.RequiredParameterError('exampleParam');
 	}, { instanceOf: errors.RequiredParameterError });
@@ -29,7 +33,7 @@ test('required parameter', t => {
 	t.is(requiredParameterError.parameter, 'exampleParam');
 });
 
-test('unknown error', t => {
+test('[error] unknown error', t => {
 	const unknownError = t.throws<errors.UnknownError>(() => {
 		throw new errors.UnknownError('what kind this error?');
 	}, { instanceOf: errors.UnknownError });
@@ -37,11 +41,23 @@ test('unknown error', t => {
 	t.is(unknownError.message, 'what kind this error?');
 });
 
-test('unknown http error', t => {
+test('[error] unknown http error', t => {
 	const unknownHTTPError = t.throws<errors.UnknownHTTPError>(() => {
 		throw new errors.UnknownHTTPError('i am teapot', 418);
 	}, { instanceOf: errors.UnknownHTTPError });
 
 	t.is(unknownHTTPError.httpCode, 418);
 	t.is(unknownHTTPError.message, 'i am teapot');
+	t.is(unknownHTTPError.shortMessage, 'Unknown HTTP Error');
+});
+
+test('[error] internal server error', t => {
+	const internalServerError = t.throws<errors.InternalServerError>(() => {
+		throw new errors.InternalServerError('Test message', 21);
+	}, { instanceOf: errors.InternalServerError });
+
+	t.is(internalServerError.httpCode, 500);
+	t.is(internalServerError.message, 'Test message');
+	t.is(internalServerError.shortMessage, 'Internal Server Error');
+	t.is(internalServerError.statusCode, 21);
 });
