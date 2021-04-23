0e5072ec51c2d603da4d4c07c4e80d5f67f1d324
Andrey Wampir <wampirchikkkk@yandex.ru>
2021-04-23 23:33:27 +0500

    ✅ test: add tests for `discover` endpoint v3

--- test/v3/discover.ts
+++ test/v3/discover.ts
@@ -3,6 +3,7 @@
 import ava, { TestInterface } from 'ava';
 
 import MovieDB from '../../src';
+import { NotEnoughPermissionError } from '../../src/errors';
 
 interface Context {
 	mdb: MovieDB;
@@ -12,6 +13,69 @@ const test = ava as TestInterface<Context>;
 
 const TMDB_ACCESS_TOKEN = process.env.TMDB_ACCESS_TOKEN as string;
 
-test.todo('[v3:discover] movie');
+test.before(t => {
+	const mdb = new MovieDB({
+		accessToken: TMDB_ACCESS_TOKEN,
+		language: 'ru-RU',
+	});
 
-test.todo('[v3:discover] tv');
+	t.context.mdb = mdb;
+});
+
+test('[v3:discover] errors', async t => {
+	const mdb = new MovieDB({ accessToken: 'fake_access_token' });
+
+	let notEnoughPermissionError = await t.throwsAsync<NotEnoughPermissionError>(
+		mdb.discover().movie(),
+		{ instanceOf: NotEnoughPermissionError },
+	);
+
+	t.is(notEnoughPermissionError.httpCode, 401);
+	t.is(notEnoughPermissionError.message, 'Invalid API key: You must be granted a valid key.');
+	t.is(notEnoughPermissionError.shortMessage, 'Not Enough Permission');
+	t.is(notEnoughPermissionError.statusCode, 7);
+
+	notEnoughPermissionError = await t.throwsAsync<NotEnoughPermissionError>(
+		mdb.discover().tv(),
+		{ instanceOf: NotEnoughPermissionError },
+	);
+
+	t.is(notEnoughPermissionError.httpCode, 401);
+	t.is(notEnoughPermissionError.message, 'Invalid API key: You must be granted a valid key.');
+	t.is(notEnoughPermissionError.shortMessage, 'Not Enough Permission');
+	t.is(notEnoughPermissionError.statusCode, 7);
+});
+
+test('[v3:discover] movie', async t => {
+	const { mdb } = t.context;
+	const discover = mdb.discover();
+
+	await t.notThrowsAsync(discover.movie());
+	await t.notThrowsAsync(mdb.discover().movie());
+
+	const movie = await discover.movie({
+		language: 'ru-RU',
+		page: 2,
+	});
+
+	t.truthy(movie);
+	t.truthy(movie.results);
+	t.is(movie.page, 2);
+});
+
+test('[v3:discover] tv', async t => {
+	const { mdb } = t.context;
+	const discover = mdb.discover();
+
+	await t.notThrowsAsync(discover.tv());
+	await t.notThrowsAsync(mdb.discover().tv());
+
+	const tv = await discover.tv({
+		language: 'ru-RU',
+		page: 2,
+	});
+
+	t.truthy(tv);
+	t.truthy(tv.results);
+	t.is(tv.page, 2);
+});
